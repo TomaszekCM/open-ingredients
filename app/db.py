b65ad369710365fc9e05aa146643e9b1cfba8466
Tomasz Praczyk <tomek.praczyk@gmail.com>
2026-06-30 17:27:40 +0200

    chore(db): initialize SQLAlchemy and Alembic

---
+++ app/db.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./openingredients.db")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
